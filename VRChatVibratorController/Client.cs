9bb4d8a4885d24ea367fbd12aceeab70a1f75f09
Eric van Fandenfart <ruhlanderic@gmail.com>
2021-06-15 18:29:18 +0200

    Update Client.cs

--- VRChatVibratorController/Client.cs
+++ VRChatVibratorController/Client.cs
@@ -9,17 +9,21 @@ namespace Vibrator_Controller
 {
     class Client
     {
-
+        public static string currentlyConnectedCode; 
         //Source https://codereview.stackexchange.com/questions/41591/websockets-client-code-and-making-it-production-ready
         static ClientWebSocket webSocket;
 
-        internal static async void setupClient(string firstMsg)
+        internal static async void setupClient(string[] queue)
         {
             try
             {
                 webSocket = new ClientWebSocket();
-                await webSocket.ConnectAsync(new Uri("wss://control.markstuff.net:8080"), CancellationToken.None);
-                await Task.WhenAll(Receive(webSocket), send(firstMsg));
+                await webSocket.ConnectAsync(new Uri("ws://control.markstuff.net:8080"), CancellationToken.None);
+                foreach (string item in queue)
+                {
+                    await send(item);
+                }
+                await Receive(webSocket);
             }
             catch (Exception ex)
             {
@@ -27,15 +31,22 @@ namespace Vibrator_Controller
             }
         }
 
+        
+
         internal static async Task send(string msg)
         {
             //MelonLogger.Log("Sending: [" + msg + "]");
-            if (webSocket == null)
+            if (webSocket == null || (webSocket.State != WebSocketState.Open && currentlyConnectedCode == null))
             {
-                setupClient(msg);
+                setupClient(new string[]{ msg });
                 return;
             }
-
+            MelonDebug.Msg(webSocket.State);
+            if (webSocket.State != WebSocketState.Open && currentlyConnectedCode != null)
+            {
+                setupClient(new string[] {"join "+ currentlyConnectedCode, msg });
+                return;
+            }
             byte[] buffer = new byte[20];
             buffer = Encoding.Unicode.GetBytes(msg);
             await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
